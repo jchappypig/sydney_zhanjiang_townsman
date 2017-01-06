ead29d6368ec2c6d84b5d7bfc7451cc50608e22f
Huanhuan Huang <hhuang@atlassian.com>
2017-01-06 20:38:24 +1100

    Added casein service.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+
+#Casein routes
+namespace :casein do
+    resources :members
+end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
